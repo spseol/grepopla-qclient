2f584afdc690f1998c6c9530f993e89f158e3c8f
Sony Pony <sirarseus@gmail.com>
2014-12-13 19:52:47 +0100

    Add C++: expand Ship class - paint, rotation

--- Grepopla/components/objects/ship/ship.cpp
+++ Grepopla/components/objects/ship/ship.cpp
@@ -1,4 +1,6 @@
 #include "ship.h"
+#include <QtMath>
+#include <QtSvg/QSvgRenderer>
 
 Ship::Ship(QQuickItem *parent) :
     AbstractItem(parent)
@@ -7,18 +9,46 @@ Ship::Ship(QQuickItem *parent) :
 
 void Ship::paint(QPainter *painter)
 {
+    //relative do not work
+    QSvgRenderer renderer(QString("C:/Users/Sony/Documents/grepopla-qclient/Grepopla/resources/images/black_arrow.svg"));
 
+    renderer.render(painter);
 }
 
-void Ship::follow(int arg)
+qreal Ship::calculateRotation()
 {
+    qreal dx = m_destination.x();
+    qreal dy = m_destination.y();
+    qreal angle = qAtan2(dx - this->x(), this->y() - dy) / M_PI * 180.0;    //in degrees
 
+    if(360 - abs(angle) > abs(angle))
+        return angle;
+    else
+        return 360 - abs(angle);
 }
+
+/*---------------------*/
+/*-------GETTERS-------*/
+/*---------------------*/
 
 int Ship::type() const
 {
     return m_type;
 }
+
+QPoint Ship::destination() const
+{
+    return m_destination;
+}
+
+QUrl Ship::imageSource() const
+{
+    return m_imageSource;
+}
+
+/*---------------------*/
+/*-------SETTERS-------*/
+/*---------------------*/
 
 void Ship::setType(int arg)
 {
@@ -27,4 +57,22 @@ void Ship::setType(int arg)
 
     m_type = arg;
     emit typeChanged(arg);
+}
+
+void Ship::setDestination(QPoint arg)
+{
+    if (m_destination == arg)
+        return;
+
+    m_destination = arg;
+    emit destinationChanged(arg);
+}
+
+void Ship::setImageSource(QUrl arg)
+{
+    if (m_imageSource == arg)
+        return;
+
+    m_imageSource = arg;
+    emit imageSourceChanged(arg);
 }
--- Grepopla/components/objects/ship/ship.h
+++ Grepopla/components/objects/ship/ship.h
@@ -11,26 +11,39 @@ class Ship : public AbstractItem
 
     Q_ENUMS(ShipType)
     Q_PROPERTY(int type READ type WRITE setType NOTIFY typeChanged)
+    Q_PROPERTY(QPoint destination READ destination WRITE setDestination NOTIFY destinationChanged)
+    Q_PROPERTY(QUrl imageSource READ imageSource WRITE setImageSource NOTIFY imageSourceChanged)
 
-    int m_type;
+    private:
+        int m_type;
+        QPoint m_destination;
+        QUrl m_imageSource;
 
     public:
         explicit Ship(QQuickItem *parent = 0);
 
         enum ShipType { SmallShip ,ColonisingShip };
+
         virtual void paint(QPainter *painter);
+        Q_INVOKABLE qreal calculateRotation();
 
         int type() const;
+        QPoint destination() const;
+        QUrl imageSource() const;
 
     signals:
-        void typeChanged(int arg);
-
         void follow(int arg);
         void isFollowed(int arg);
         void isDisfollowed(int arg);
 
+        void imageSourceChanged(QUrl arg);
+        void typeChanged(int arg);
+        void destinationChanged(QPoint arg);
+
     public slots:
         void setType(int arg);
+        void setDestination(QPoint arg);
+        void setImageSource(QUrl arg);
 };
 
 
