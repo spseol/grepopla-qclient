f184bcb9e85b1768d6db627ff3ea0547e72107eb
Sony Pony <sirarseus@gmail.com>
2014-12-18 16:29:08 +0100

    Add C++: new properties - Planet

--- Grepopla/components/objects/planet/planet.cpp
+++ Grepopla/components/objects/planet/planet.cpp
@@ -9,3 +9,43 @@ void Planet::paint(QPainter *painter)
 {
 
 }
+
+Player *Planet::owner() const
+{
+    return m_owner;
+}
+
+int Planet::population() const
+{
+    return m_population;
+}
+
+void Planet::buildShip(ShipOptions *type)
+{
+
+}
+
+//void Planet::buildShip(ShipOptions *type){}
+
+void Planet::handleCapturing(Player *conquerer)
+{
+
+}
+
+void Planet::setOwner(Player *arg)
+{
+    if (m_owner == arg)
+        return;
+
+    m_owner = arg;
+    emit ownerChanged(arg);
+}
+
+void Planet::setPopulation(int arg)
+{
+    if (m_population == arg)
+        return;
+
+    m_population = arg;
+    emit populationChanged(arg);
+}
--- Grepopla/components/objects/planet/planet.h
+++ Grepopla/components/objects/planet/planet.h
@@ -2,16 +2,45 @@
 #define PLANET_H
 
 #include <QQuickPaintedItem>
+#include <QTimer>
 #include "../abstractitem.h"
+#include "../../../components/game/player/player.h"
+#include "../../game/game.h"
 
 class Planet : public AbstractItem
 {
     Q_OBJECT
 
+    Q_PROPERTY(Player* owner READ owner WRITE setOwner NOTIFY ownerChanged)
+    Q_PROPERTY(int population READ population WRITE setPopulation NOTIFY populationChanged)
+
+    private:
+        Player* m_owner;
+        QTimer m_buildTimer;
+        QTimer m_capturingTimer;
+        int m_population;
+
     public:
         explicit Planet(QQuickItem *parent = 0);
 
         virtual void paint(QPainter *painter);
+
+        Player* owner() const;
+        int population() const;
+
+    public slots:
+        void buildShip(ShipOptions *type);
+        void handleCapturing(Player* conquerer);
+
+        void setOwner(Player* arg);
+        void setPopulation(int arg);
+
+    signals:
+        void shipFinished(int ID);
+        //void buildShip();
+
+        void ownerChanged(Player* arg);
+        void populationChanged(int arg);
 };
 
 #endif // PLANET_H
